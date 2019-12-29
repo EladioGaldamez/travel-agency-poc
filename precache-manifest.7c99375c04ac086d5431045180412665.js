498c7122a5bc08ca3d5536c2fc68ff37be96619f
José Galdámez <eladio.galdamez@gmail.com>
2019-12-28 23:48:52 -0600

    Updates

---
+++ precache-manifest.7c99375c04ac086d5431045180412665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc9f1d470b3f3c7ec30bfbf4a67eb5b",
+    "url": "/travel-agency-poc/index.html"
+  },
+  {
+    "revision": "fba5a0437368da2d6396",
+    "url": "/travel-agency-poc/static/css/main.417d0813.chunk.css"
+  },
+  {
+    "revision": "7e9f4db36f7877d94600",
+    "url": "/travel-agency-poc/static/js/2.efca4a5d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/travel-agency-poc/static/js/2.efca4a5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fba5a0437368da2d6396",
+    "url": "/travel-agency-poc/static/js/main.f2bcb59c.chunk.js"
+  },
+  {
+    "revision": "46c629f9823e0d6c6d52",
+    "url": "/travel-agency-poc/static/js/runtime-main.d1ea1377.js"
+  }
+]);
